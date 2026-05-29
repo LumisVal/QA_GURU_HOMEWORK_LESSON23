3bd8f112b6912316cebd12d07c53c67b7a5c8290
LEONID <leonid.gromov.2016@mail.ru>
2026-05-29 23:45:46 +0300

    2.5 update

--- utils/attachments.py
+++ utils/attachments.py
@@ -9,3 +9,27 @@ def add_screenshot(name="Screenshot"):
         name=name,
         attachment_type=AttachmentType.PNG,
     )
+
+
+def add_page_source():
+    allure.attach(
+        browser.config.driver.page_source,
+        name="Page Source",
+        attachment_type=AttachmentType.XML,
+    )
+
+
+def add_browserstack_session_link(session_id):
+    allure.attach(
+        f"https://app-automate.browserstack.com/dashboard/v2/sessions/{session_id}",
+        name="BrowserStack Session",
+        attachment_type=AttachmentType.TEXT,
+    )
+
+
+def add_browserstack_video(session_id):
+    allure.attach(
+        f"https://app-automate.browserstack.com/dashboard/v2/sessions/{session_id}",
+        name="BrowserStack Video",
+        attachment_type=AttachmentType.TEXT,
+    )
