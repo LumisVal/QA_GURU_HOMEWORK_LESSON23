7bd30fe6ede375870d1c403314257100877260df
LEONID <leonid.gromov.2016@mail.ru>
2026-05-29 23:06:54 +0300

    1.4

--- pages/onboarding_page.py
+++ pages/onboarding_page.py
@@ -1,24 +1,34 @@
+import allure
 from selene import browser, have
 
 
-# noinspection PyMethodMayBeStatic
 class OnboardingPage:
 
+    @allure.step("Проверить первый экран")
     def should_see_first_screen(self):
         browser.element(
             ("id", "org.wikipedia.alpha:id/primaryTextView")
-        ).should(have.text("The Free Encyclopedia"))
+        ).should(
+            have.text("The Free Encyclopedia")
+        )
 
+    @allure.step("Проверить второй экран")
     def should_see_second_screen(self):
         browser.element(
             ("id", "org.wikipedia.alpha:id/primaryTextView")
-        ).should(have.text("New ways to explore"))
+        ).should(
+            have.text("New ways to explore")
+        )
 
+    @allure.step("Проверить третий экран")
     def should_see_third_screen(self):
         browser.element(
             ("id", "org.wikipedia.alpha:id/primaryTextView")
-        ).should(have.text("Reading lists"))
+        ).should(
+            have.text("Reading lists")
+        )
 
+    @allure.step("Проверить четвертый экран")
     def should_see_fourth_screen(self):
         browser.element(
             ("id", "org.wikipedia.alpha:id/primaryTextView")
@@ -26,11 +36,13 @@ class OnboardingPage:
             have.text("Data & Privacy")
         )
 
+    @allure.step("Нажать Continue")
     def continue_click(self):
         browser.element(
             ("id", "org.wikipedia.alpha:id/fragment_onboarding_forward_button")
         ).click()
 
+    @allure.step("Нажать Get Started")
     def get_started(self):
         browser.element(
             ("id", "org.wikipedia.alpha:id/fragment_onboarding_done_button")
